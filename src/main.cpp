60ac492385f2e7650c3672dfadf36a67a3bc9532
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-10-25 11:45:18 +0200

    moved logic to specific controllers

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,17 @@
+#include "GaiaWebOptions.h"
 #include <drogon/drogon.h>
+#include <HttpSessionEvents.h>
 
-int main() {
-    drogon::app().registerHandler(
-        "/upload_endpoint",
-        [](const drogon::HttpRequestPtr &req,
-           std::function<void(const drogon::HttpResponsePtr &)> &&callback) {
-            drogon::MultiPartParser fileUpload;
-            if (fileUpload.parse(req) != 0 || fileUpload.getFiles().size() != 1)
-            {
-                auto resp = drogon::HttpResponse::newHttpResponse();
-                resp->setBody("Must only be one file");
-                resp->setStatusCode(drogon::k403Forbidden);
-                callback(resp);
-                return;
-            }
 
-            auto &file = fileUpload.getFiles()[0];
-            auto md5 = file.getMd5();
-            auto resp = drogon::HttpResponse::newHttpResponse();
-            resp->setBody(
-                "The server has calculated the file's MD5 hash to be " + md5);
-            file.save();
-            LOG_INFO << "The uploaded file has been saved to the ./uploads directory";
-            callback(resp);
-        },
-        {drogon::Post});
+int main( [[maybe_unused]] int argc, [[maybe_unused]] const char* argv[] ) {
+  // Initialize command line options
+  GaiaWebOptions::getInstance().init( argc, argv );
 
-    //Load config file
-    drogon::app().loadConfigFile("config.json");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
-    return 0;
+  drogon::app().setSessionEventsHandler( std::make_unique<HttpSessionEvents>() );
+  
+  //Load config file
+  drogon::app().loadConfigFile( GaiaWebOptions::getInstance().getConfigFilename() );
+  //Run HTTP framework,the method will block in the internal event loop
+  drogon::app().run();
+  return 0;
 }
