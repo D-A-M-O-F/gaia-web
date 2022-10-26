ffc71e994431cf0d4bf2622815f1f78f75a1e30f
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-10-26 14:20:20 +0200

    allow to specify webroot from command line options. If option is also specified in configuration file, then this last will have priority

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@ int main( [[maybe_unused]] int argc, [[maybe_unused]] const char* argv[] ) {
   GaiaWebOptions::getInstance().init( argc, argv );
 
   drogon::app().setSessionEventsHandler( std::make_unique<HttpSessionEvents>() );
-  
+  drogon::app().setDocumentRoot( GaiaWebOptions::getInstance().getWebRootPath() );
+
   //Load config file
   drogon::app().loadConfigFile( GaiaWebOptions::getInstance().getConfigFilename() );
+
   //Run HTTP framework,the method will block in the internal event loop
   drogon::app().run();
   return 0;
