d3f97058b309cf91bdf94748fc489dd34b2a5680
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-11-03 12:39:09 +0100

    add defines file

---
+++ include/GaiaWebDefines.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define GTFS_INPUT         "gtfs_in"
+#define GTFS_OUTPUT        "gtfs_out"
+#define GTFS_LOG           "log"
+
+#define GAIA_CFG           "cfg"
+#define GAIA_RCFG          "rcfg"
+#define GAIA_BIN           "bin"
+#define GAIA_SCRIPTS       "scripts"
+
+#define WWW_PROCESSING     "processing"
+#define WWW_ULOADS         "uploads"
