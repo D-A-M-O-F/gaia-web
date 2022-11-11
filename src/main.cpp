17ec621bde3a3b1753dbee5ef329ff3aab1d8d03
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2022-11-11 20:12:10 +0100

    nop

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main( [[maybe_unused]] int argc, [[maybe_unused]] const char* argv[] ) {
 
   //Load config file
   drogon::app().loadConfigFile( GaiaWebOptions::getInstance().getWebConfigFilename() );
-
+  
   //Run HTTP framework,the method will block in the internal event loop
   drogon::app().run();
   return 0;
